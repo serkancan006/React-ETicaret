554fd28c1817da07221c78a61b166cb103fb605a
Serkan Can Kaya <109299838+serkancan006@users.noreply.github.com>
2024-05-19 10:13:56 +0300

    search modal yapimi

--- frontend/src/components/Layout/Header/Header.jsx
+++ frontend/src/components/Layout/Header/Header.jsx
@@ -1,6 +1,6 @@
 import "./Header.css";
 
-const Header = () => {
+const Header = ({ setIsSearchShow }) => {
   return (
     <header>
       <div className="global-notification">
@@ -164,7 +164,7 @@ const Header = () => {
                             href="#"
                             className="megamenu-single-button btn btn-sm"
                           >
-                             Shop Now
+                            Shop Now
                           </a>
                         </div>
                       </div>
@@ -189,7 +189,7 @@ const Header = () => {
                 <a href="account.html" className="header-account">
                   <i className="bi bi-person"></i>
                 </a>
-                <button className="search-button">
+                <button className="search-button" onClick={() => setIsSearchShow(true)} >
                   <i className="bi bi-search"></i>
                 </button>
                 <a href="#">
