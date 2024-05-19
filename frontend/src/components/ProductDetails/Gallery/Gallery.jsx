cf0431e1cb23f5c7cb2234accbecb3b2920fac38
Serkan Can Kaya <109299838+serkancan006@users.noreply.github.com>
2024-05-19 21:27:22 +0300

    react sayfalama

--- frontend/src/components/ProductDetails/Gallery/Gallery.jsx
+++ frontend/src/components/ProductDetails/Gallery/Gallery.jsx
@@ -61,7 +61,7 @@ const Gallery = () => {
     return (
         <div className="product-gallery">
             <div className="single-image-wrapper">
-                <img src={activeImg.img} id="single-image" alt="" />
+                <img src={`/${activeImg.img}`} id="single-image" alt="" />
             </div>
             <div className="product-thumb">
                 <div className="glide__track" data-glide-el="track">
@@ -72,7 +72,7 @@ const Gallery = () => {
                                     img: productsData[0].img.thumbs[index],
                                     imgIndex: index,
                                 })}>
-                                    <img src={itemImg} alt="" className={`img-fluid ${activeImg.imgIndex == index ? "active" : ""}`} />
+                                    <img src={`/${itemImg}`} alt="" className={`img-fluid ${activeImg.imgIndex == index ? "active" : ""}`} />
                                 </li>
                             ))}
                         </Slider>
