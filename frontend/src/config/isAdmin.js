7bc2e509a288216a0899e3637da530af29db189f
Serkan Can Kaya <109299838+serkancan006@users.noreply.github.com>
2024-05-23 15:42:43 +0300

    ant design ile admin paneli yapimi

---
+++ frontend/src/config/isAdmin.js
@@ -0,0 +1,2 @@
+export const isAdmin = window.location.pathname.startsWith("/admin");
+//console.log(window);
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,20 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from "react-router-dom"
-import MainLayout from './layouts/MainLayout.jsx'
+import { Layout } from './layouts/Layout.jsx';
 import CartProvider from './context/CartProvider.jsx';
 import App from './App'
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import './index.css'
+import './index.css';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <CartProvider>
-        <MainLayout>
+        <Layout>
           <App />
-        </MainLayout>
+        </Layout>
       </CartProvider>
     </BrowserRouter>
   </React.StrictMode>
